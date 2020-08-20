70cb1cd27c50db5d73a338a48c32bdaa7a4fee16
agendreau <alex.gendreau24@gmail.com>
2020-08-20 13:29:02 -0600

    cleaning up

--- Tutorial_Environmental_Data.md
+++
@@ -1,1 +0,0 @@
-# Tutorial_Environmental_Data
--- magnetometer_metal_detector.ts
+++
@@ -1,8 +0,0 @@
-input.onButtonPressed(Button.A, function () {
-    led.setBrightness(input.magneticForce(Dimension.Strength))
-    basic.showIcon(IconNames.Square)
-    if (input.magneticForce(Dimension.Strength) > 250) {
-        music.playTone(659, music.beat(BeatFraction.Whole))
-    }
-    basic.showNumber(Math.round(input.magneticForce(Dimension.Strength)))
-})
